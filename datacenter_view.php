401b9d9f5caa80c9f2b395e22c71cafe038d3e6e
sergioecm60 <sergiomiers@gmail.com>
2025-10-23 13:35:03 -0300

    update

--- datacenter_view.php
+++ datacenter_view.php
@@ -585,7 +585,7 @@ if (isset($_GET['debug']) && $_SESSION['user_role'] === 'admin') {
                         <div class="server-header">
                             <div class="server-main-info">
                                 <?php
-                                    $icons = ['physical' => '🖥️', 'virtual' => '🐳', 'container' => '📦', 'cloud' => '☁️', 'isp' => '🌐', 'switch' => '🔀', 'router' => '🧱', 'dvr' => '📼', 'alarmas' => '🚨', 'antena' => '📡','Wifi' => '🛜'];
+                                    $icons = ['physical' => '🖥️', 'virtual' => '🐳', 'container' => '📦', 'cloud' => '☁️', 'isp' => '🌐', 'switch' => '🔀', 'router' => '🧱', 'dvr' => '📼', 'alarmas' => '🚨', 'antena' => '📡','wifi' => '🛜'];
                                     $icon = $icons[strtolower($server['type'] ?? 'physical')] ?? '⚙️';
                                 ?>
                                 <div class="server-icon server-type-<?= strtolower($server['type'] ?? 'physical') ?>">
